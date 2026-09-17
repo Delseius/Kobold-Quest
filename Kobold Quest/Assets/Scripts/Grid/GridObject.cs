c8e146fd1017157d608abff97d6c429ae51db519
Delseius <nuke300and1@gmail.com>
2026-09-17 14:52:48 +1200

    GridObject Modifications and GridObjectType
    Made changes to the structure of the GridObject.cs file and created a GridObjectType.cs file to support it.

--- Kobold Quest/Assets/Scripts/Grid/GridObject.cs
+++ Kobold Quest/Assets/Scripts/Grid/GridObject.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public abstract class GridObject : MonoBehaviour
 {
+    [SerializeField]
+    private GridObjectType objectType = GridObjectType.WorldObject;
+
     public Vector2Int GridPosition { get; private set; }
 
     protected virtual void Start()
@@ -13,37 +16,104 @@ public abstract class GridObject : MonoBehaviour
     {
         if (GridSpace.Instance == null)
         {
-            Debug.LogError(
-                "No GridSpace exists in the scene."
-            );
-
+            Debug.LogError("No GridSpace exists in the scene.");
             return;
         }
 
         GridPosition =
             GridSpace.Instance.WorldToGrid(transform.position);
+
+        GridCell cell =
+            GridSpace.Instance.GetCell(GridPosition);
+
+        switch (objectType)
+        {
+            case GridObjectType.WorldObject:
+                cell.WorldObject = gameObject;
+                break;
+
+            case GridObjectType.Block:
+                cell.Block = gameObject;
+                break;
+
+            case GridObjectType.Item:
+                cell.Item = gameObject;
+                break;
+        }
+    }
+
+    protected virtual void UnregisterFromGrid()
+    {
+        if (GridSpace.Instance == null)
+        {
+            return;
+        }
+
+        GridCell cell =
+            GridSpace.Instance.GetCell(GridPosition);
+
+        switch (objectType)
+        {
+            case GridObjectType.WorldObject:
+
+                if (cell.WorldObject == gameObject)
+                {
+                    cell.WorldObject = null;
+                }
+
+                break;
+
+            case GridObjectType.Block:
+
+                if (cell.Block == gameObject)
+                {
+                    cell.Block = null;
+                }
+
+                break;
+
+            case GridObjectType.Item:
+
+                if (cell.Item == gameObject)
+                {
+                    cell.Item = null;
+                }
+
+                break;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        UnregisterFromGrid();
     }
 
     public void SnapToGrid()
     {
         if (GridSpace.Instance == null)
         {
-            Debug.LogError(
-                "No GridSpace exists in the scene."
-            );
-
+            Debug.LogError("No GridSpace exists in the scene.");
             return;
         }
+
+        UnregisterFromGrid();
 
         GridPosition =
             GridSpace.Instance.WorldToGrid(transform.position);
 
         transform.position =
             GridSpace.Instance.GridToWorld(GridPosition);
+
+        RegisterWithGrid();
     }
 
     public Vector3 GetGridWorldPosition()
     {
+        if (GridSpace.Instance == null)
+        {
+            return transform.position;
+        }
+
         return GridSpace.Instance.GridToWorld(GridPosition);
     }
 }
