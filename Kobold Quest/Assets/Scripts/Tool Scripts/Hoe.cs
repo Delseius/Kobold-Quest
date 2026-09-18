beac076b47ba1910d6c3035770fe5a3173ab3a38
CJ6-exe <TremorTusk6@gmail.com>
2026-09-18 17:56:59 +1200

    Added Hoe functionality
    Added frameworks and hoe functionality, needs checkspace

--- Kobold Quest/Assets/Scripts/Tool Scripts/Hoe.cs
+++ Kobold Quest/Assets/Scripts/Tool Scripts/Hoe.cs
@@ -1,3 +1,4 @@
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Hoe : Tool
@@ -8,17 +9,32 @@ public class Hoe : Tool
     [Header("Tool Toughness")]
     [SerializeField]
     public int toughness;
-
+    [SerializeField]
+    private GameObject farmPlotPrefab;
     public void hoeUse()
     {
         if(toolDurability > 0) //check durability
         {
-            //Code to make the block tilled
-            toolDurability -= 1; //Reduce durability
+            //Code to check if it can be tilled
+            if(checkSpace() == 1)
+            {
+                spawnFarmPlot();
+                toolDurability -= 1; //Reduce durability
+            } else
+            {
+                
+            }
+            
         } else
         {
             //Show that hoe is broken
             return;
         }
+    }
+    
+    public virtual void spawnFarmPlot(Vector2 spawnPosition)
+    {
+        //string path = "Assets/Prefabs/Farming/FarmPlot.prefab";
+        Instantiate(farmPlotPrefab, spawnPosition, Quaternion.identity);
     }
 }
