3781baf60ad2c9140b724fb55d06c769c8180fc8
CJ6-exe <TremorTusk6@gmail.com>
2026-09-18 20:33:56 +1200

    Sharpened Toothpick
    Added some of the functionality for Pickaxe

--- Kobold Quest/Assets/Scripts/Items/Tool Scripts/Pickaxe.cs
+++ Kobold Quest/Assets/Scripts/Items/Tool Scripts/Pickaxe.cs
@@ -12,15 +12,40 @@ public class Pickaxe : Tool
     
     public void pickaxeUse()
     {
-        if(toolDurability > 0) //check durability
+        // Check durability.
+        if (toolDurability <= 0)
         {
-            //Code to make the block loose or destroy the loose block
-            toolDurability -= 1; //Reduce durability
-        } else
-        {
-            //Show that pickaxe is broken
+            Debug.Log("Pickaxe is broken.");
             return;
         }
+
+        // Find the grid cell in front of the kobold.
+        GridCell targetCell =
+            CheckSpace();
+
+        if (targetCell == null)
+        {
+            Debug.Log(
+                "Pickaxe cannot be used: " +
+                "there is no valid target cell."
+            );
+
+            return;
+        }
+
+        // Check that the grid cell is a block that can be destroyed.
+
+
+        // Make the block able to be picked up.
+        
+
+        // Reduce durability only after a successful use.
+        toolDurability -= toughness;
+
+        Debug.Log(
+            "Pickaxe successfully used on grid cell " +
+            targetCell.Position
+        );
     }
 
 }
